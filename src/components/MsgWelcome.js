4a64b0f51027c5d6f934cc661b9d88f1e662d7c2
nanofleck <fernafleck@gmail.com>
2020-09-14 17:53:07 -0300

    Creacion de proyecto bikebike con react

---
+++ src/components/MsgWelcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Welcome from './Welcome.js';
+
+function MsgWelcome() {
+    return (
+      <div>
+          <p>Este es el mensaje de...</p>
+          <Welcome />
+      </div>
+    );
+  }
+
+export default MsgWelcome;
---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Welcome() {
+    return (
+      <h2>¡¡¡Bienvenida a BIKEBIKE!!!</h2>
+    );
+  }
+
+export default Welcome;
