37dbc1da38f93b479986f5a792bf2dabbb5ad9db
nanofleck <fernafleck@gmail.com>
2020-09-28 19:47:40 -0300

    Insercion de Imagenes

--- src/components/MsgWelcome.js
+++ src/components/MsgWelcome.js
@@ -6,6 +6,8 @@ function MsgWelcome() {
       <div>
           <p>Este es el mensaje de...</p>
           <Welcome />
+          <img src='./img/7513.jpg' style={{width: "100%"}} />
+          <a href='https://www.freepik.es/fotos/viajes'>Foto de Viajes creado por jcomp - www.freepik.es</a>
       </div>
     );
   }
